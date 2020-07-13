9dfed26a0172b4874d8ad5742aab2ba50dae7ef9
NiiRoZz <extazziii1000@gmail.com>
2020-07-13 22:25:15 +0200

    Remove simplexNoise part 3

--- Source/MineUE4/MineUE4.Build.cs
+++ Source/MineUE4/MineUE4.Build.cs
@@ -8,7 +8,7 @@ public class MineUE4 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "SimplexNoise" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] { "ProceduralMeshComponent" });
 
